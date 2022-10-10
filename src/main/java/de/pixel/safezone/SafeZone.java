37587ca0381c48c8f2bb503ae29aa7e9b7f49260
Pixel <mr.lars@outlook.de>
2022-10-10 10:33:52 +0200

    Initial commit

---
+++ src/main/java/de/pixel/safezone/SafeZone.java
@@ -0,0 +1,17 @@
+package de.pixel.safezone;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SafeZone extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
